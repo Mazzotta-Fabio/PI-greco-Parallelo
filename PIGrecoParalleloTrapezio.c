6005ef78b517e7e27da02d5e54767eca21bbff6f
Mazzotta-Fabio <33379712+Mazzotta-Fabio@users.noreply.github.com>
2018-12-26 11:32:13 +0100

    Update PIGrecoParalleloTrapezio.c

--- PIGrecoParalleloTrapezio.c
+++ PIGrecoParalleloTrapezio.c
@@ -30,8 +30,6 @@ int main(int argc, char* argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-
-
 	if(my_rank==0){
 		n=atoi(argv[argc-1]);
 		arrayValoriInizio=(int *)malloc(p *sizeof(int));
